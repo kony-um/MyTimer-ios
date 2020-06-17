63d148462e38babddc7f65d80420bec96d3a362b
ymk1t <yuma1102eff@gmail.com>
2020-06-17 23:39:15 +0900

    Initial Commit

---
+++ MyTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by 小西裕真 on 2020/06/17.
+//  Copyright © 2020 小西裕真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
